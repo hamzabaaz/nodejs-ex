3a753af8139faa57d46639530e94553d7a912558
Othmane Ghazi <othmane@skrapp.io>
2017-06-12 08:11:25 +0200

    wip

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ var express = require('express'),
     fs      = require('fs'),
     app     = express(),
     eps     = require('ejs'),
-    bodyParser = require('body-parser'),
     morgan  = require('morgan');
     
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-app.use(bodyParser.json())
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
